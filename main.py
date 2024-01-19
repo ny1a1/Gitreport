eecded342a07c3e3f2841e159e35590e242207fa
layz <broitzlayz@gmail.com>
2024-01-20 01:10:49 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from time import sleep
 from random import randint
 from datetime import datetime
 
-list = [2, 4, 6, 8, 10]
+mylist = [2, 4, 6, 8, 10]
 c = randint(1,11)
 for i in range(randint(1, 15)):
     a = randint(1, 10)
@@ -25,9 +25,10 @@ print("Text")
 sleep(0.1)
 print("Text 222")
 print(datetime.today())
-if c in list:
+
+if c in mylist:
     print("Omg!")
-elif 5 in list:
+elif 5 in mylist:
     print("Ok!")
 else:
     print("Hello Ukrainian people!")
